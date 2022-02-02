d8e704b46ff834f279ec4097e68eb3a555dee9bc
Justyna <justynas.front.end.developer@gmail.com>
2022-02-02 17:28:36 +0000

    update

--- src/Form.js
+++ src/Form.js
@@ -1,6 +1,30 @@
-import React from 'react';
+import React,{useState} from 'react';
+
+const encode = (data) => {
+    return Object.keys(data)
+      .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key])).join('&');
+  }
 
 function Form() {
+
+
+
+    const [state, setState] = useState({name: '', email: '', message: '' })
+
+    const handleChange = e =>
+      setState({...state, [e.target.name]: e.target.value })
+  
+    const handleSubmit = e => {
+      fetch('/', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: encode({ 'form-name': 'contactForm', ...state })
+      })
+        .then(() => alert('Success!'))
+        .catch(error => alert(error))
+      e.preventDefault()
+    }
+  
     
     
 
@@ -21,28 +45,31 @@ function Form() {
     </div>
             
     <form 
-      method='POST' 
-      name='contactform' 
-      className='contactForm'>
+      className='contactForm' 
+      onSubmit={handleSubmit}>
 
       <input 
         type='text' 
         name='name' 
-        placeholder='Enter your name' />
+        value={state.name}
+        placeholder='Enter your name'
+        onChange={handleChange} />
 
       <input 
         type='email' 
         name='email' 
-        placeholder='Enter your email' />
+        value={state.email}
+        placeholder='Enter your email'
+        onChange={handleChange} />
 
       <textarea 
         name='message' 
-        placeholder='Messaage'></textarea>
-
+        placeholder='Messaage'
+        value={state.message}
+        onChange={handleChange}></textarea>
       <button type='submit'>Submit</button>
-
     </form>
-
+  
 
 </div>
 </div>
